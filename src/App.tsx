8fbfc5f8c905cf7bd7d41c358f8b185785a6763b
ArnavBansal <>
2023-07-24 05:33:49 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,30 @@ import 'websocket-polyfill'
 
 
 export const RELAYS = [
-    "wss://nostr-pub.wellorder.net",
+    "wss://relay.damus.io",
     "wss://nostr.drss.io",
     "wss://nostr.swiss-enigma.ch",
-    "wss://relay.damus.io",
+    "wss://relay.f7z.io",
 ];
-
-const relayPool = new RelayPool(RELAYS);
 
 function App() {
     // Replit Stuff: Handshake status, error (if any), and Replit API wrapper
-    const { status, error, replit } = useReplit();
-    if (status === "error") return <div className="text-red-500">{error?.message}</div>;
-    if (status === "loading") return <div>Loading...</div>;
+    // const { status, error, replit } = useReplit();
+    // if (status === "error") return <div className="text-red-500">{error?.message}</div>;
+    // if (status === "loading") return <div>Loading...</div>;
 
     const [sk, setSk] = useState(null);
     const [pk, setPk] = useState<string>("");
     const [shortNpub, setShortNpub] = useState<string>("");
-    const [story, setStory] = useState<string>("");
+    const [userInput, setUserInput] = useState<string>("");
     const [eventFeed, setEventFeed] = useState<Event[]>([]);
     const [relay, setRelay] = useState(null);
     const [relayUrl, setRelayUrl] = useState(RELAYS[0]);
     const [relayStatus, setRelayStatus] = useState('Connecting...');
+    const [publishedJobIDs, setPublishedJobIDs] = useState<string[]>([]);
 
     useEffect(() => {
         const _relay = relayInit(relayUrl);
-        setRelay(_relay);
 
         _relay.on('connect', () => {
             console.log(`connected to ${_relay.url}`);
@@ -44,19 +42,29 @@ function App() {
         });
 
         _relay.connect().then(() => {
-            let sub = _relay.sub([
-                {
-                    kinds: [1],
-                    authors: [pk]
-                }
-            ]);
+            let sub = _relay.sub(
+                [
+                    {
+                        kinds: [68001],
+                    }
+                ]
+            )
             sub.on('event', event => {
+                // Extract the customer's pubkey from the incoming event
+                console.log("saw event", event)
+                const incomingPubkey = event.tags.find(tag => tag[0] === 'p')?.[1];
+                // Check if the incoming pubkey matches our pubkey
+                if (incomingPubkey !== pk) return;
+
                 const eventExists = eventFeed.find(e => e.id === event.id);
-                if (!eventExists) {
-                    setEventFeed([event, ...eventFeed]);
-                }
+                setEventFeed([event, ...eventFeed]);
             });
+        }).catch((error) => {
+            console.log(`Failed to connect to ${_relay.url}: ${error}`);
+            setRelayStatus('Failed to connect');
         });
+
+        setRelay(_relay);
 
         return () => {
             _relay.close();
@@ -77,7 +85,12 @@ function App() {
         const npub = localStorage.getItem("npub");
         if (npub) {
             setPk(nip19.decode(npub).data);
-            const short = npub.slice(0, 6) + "..." + npub.slice(-6);
+            let short;
+            if (npub) {
+                short = npub.slice(0, 6) + "..." + npub.slice(-6);
+            } else {
+                short = "";
+            }
             setShortNpub(short);
         }
     }, []);
@@ -90,7 +103,12 @@ function App() {
         localStorage.setItem("npub", nip19.npubEncode(pk));
         setPk(pk);
         const npub = nip19.npubEncode(pk);
-        const short = npub.slice(0, 6) + "..." + npub.slice(-6);
+        let short;
+        if (npub) {
+            short = npub.slice(0, 6) + "..." + npub.slice(-6);
+        } else {
+            short = "";
+        }
         setShortNpub(short);
 
     };
@@ -104,26 +122,38 @@ function App() {
     }
 
     const handleSubmit = async () => {
-        if (!relay || !story || !sk) return;
+        if (!userInput || !sk || !relay) return;
         let event = {
-            kind: 1,
+            kind: 68002,
             pubkey: pk,
             created_at: Math.floor(Date.now() / 1000),
-            tags: [],
-            content: story
+            tags: [
+                ["j", "summarize"]
+            ],
+            content: userInput
         }
         event.id = getEventHash(event);
         event.sig = getSignature(event, sk);
         let pub = relay.publish(event);
+        console.log("pk ", pk);
         pub.on('ok', () => {
             console.log(`${relay.url} has accepted our event`);
-            setEventFeed(prevEvents => [...prevEvents, event]);
-        });
-        pub.on('failed', reason => {
-            console.log(`failed to publish to ${relay.url}: ${reason}`);
-        });
-        setStory("");
+            setUserInput("");
+        })
     }
+
+    const handleZap = async (e: Event) => {
+        if (window.webln === "undefined") {
+            alert("You need to use a webln enabled browser or extension to zap for these jobs! Download Alby at https://getalby.com !");
+            return;
+        }
+
+        await window.webln.enable();
+        const invoice = e.tags.find(tag => tag[0] === 'amount')?.[2];
+        const { preimage } = await window.webln.sendPayment(invoice)
+        console.log(preimage);
+    }
+
 
     return (
         <main className="flex flex-col items-center min-h-screen bg-gray-800 text-white px-4 pt-6">
@@ -166,9 +196,9 @@ function App() {
                 <input
                     type="text"
                     className="flex-grow p-2 rounded-l-full"
-                    placeholder="Write me a story"
-                    value={story}
-                    onChange={(e) => setStory(e.target.value)}
+                    placeholder="Fix my janky code..."
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
                 />
                 <button
                     className="px-2 py-1 rounded-r-full text-gray-800 hover:bg-gray-300 transition duration-200"
@@ -178,18 +208,35 @@ function App() {
                 </button>
             </form>
 
-            <div className="w-full max-w-lg p-4 bg-white rounded-lg text-gray-800 my-4">
-                {<div className="w-full max-w-lg p-4 bg-white rounded-lg text-gray-800 my-4">
-                    {eventFeed.map((event, index) => (
-                        <div key={index} className="border-b border-gray-200 p-2">
-                            <div><strong>Kind:</strong> {event.kind}</div>
-                            <div><strong>From:</strong> {event.pubkey}</div>
-                            <div><strong>Content:</strong> {event.content}</div>
-                        </div>
-                    ))}
-                </div>}
-            </div>
-        </main>
+            {eventFeed.length > 0 && (
+                <div className="w-full max-w-lg p-4 bg-white rounded-lg text-gray-800 my-4 overflow-y-auto">
+                    {eventFeed.map((event, index) => {
+                        // Extract the necessary tag data
+                        const pTag = event.tags.find(tag => tag[0] === 'p')?.[1];
+                        let shortPubkey = '';
+                        if (pTag) {
+                            shortPubkey = '@' + pTag.slice(0, 6) + '...' + pTag.slice(-6);
+                        }
+                        const amountSats = event.tags.find(tag => tag[0] === 'amount')?.[1] / 1000;
+
+                        return (
+                            <div key={index} className="border-b border-gray-200 p-2 flex justify-between items-center">
+                                <div>
+                                    <div><strong>From:</strong> {shortPubkey}</div>
+                                    <div><strong>Content:</strong> {event.content}</div>
+                                </div>
+                                <button
+                                    className="px-2 py-1 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition duration-200"
+                                    onClick={() => handleZap(event)} // assuming you're passing the event id to handleZap
+                                >
+                                    Zap ⚡ {amountSats}
+                                </button>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
+        </main >
 
 
     );
