9c58f6a0536bcdf9fde3d11a39f360d5b461f161
ArnavBansal <>
2023-07-25 22:03:52 +0000

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useReplit } from '@replit/extensions-react';
 import { useState, useEffect } from 'react';
 import './App.css'
-import { nip19, generatePrivateKey, getPublicKey, Event, getEventHash, getSignature, relayInit } from 'nostr-tools';
+import { nip19 } from 'nostr-tools';
 import 'websocket-polyfill'
-
+import NDK from '@nostr-dev-kit/ndk';
+import { NDKNip07Signer, NDKEvent, NDKSubscription } from '@nostr-dev-kit/ndk';
+import type { NostrEvent } from '@nostr-dev-kit/ndk';
 
 export const RELAYS = [
     "wss://relay.damus.io",
-    "wss://nostr.drss.io",
+    // "wss://nostr.drss.io",
     "wss://nostr.swiss-enigma.ch",
     "wss://relay.f7z.io",
 ];
@@ -18,56 +20,101 @@ function App() {
     // if (status === "error") return <div className="text-red-500">{error?.message}</div>;
     // if (status === "loading") return <div>Loading...</div>;
 
-    const [sk, setSk] = useState(null);
     const [pk, setPk] = useState<string>("");
     const [shortNpub, setShortNpub] = useState<string>("");
     const [userInput, setUserInput] = useState<string>("");
-    const [eventFeed, setEventFeed] = useState<Event[]>([]);
+    const [eventFeed, setEventFeed] = useState<NDKEvent[]>([]);
+    const [showEventFeed, setShowEventFeed] = useState<boolean>(false);
+    const [ndk, setNDK] = useState<NDK>();
     const [relay, setRelay] = useState(null);
-    const [relayUrl, setRelayUrl] = useState(RELAYS[0]);
-    const [relayStatus, setRelayStatus] = useState('Connecting...');
+    const [relayUrl, setRelayUrl] = useState(RELAYS[3]);
+    const [relayStatus, setRelayStatus] = useState('Initially Not connected...');
     const [publishedJobIDs, setPublishedJobIDs] = useState<string[]>([]);
+    const [sub, setSub] = useState<NDKSubscription | null>(null);
 
     useEffect(() => {
-        const _relay = relayInit(relayUrl);
 
-        _relay.on('connect', () => {
-            console.log(`connected to ${_relay.url}`);
-            setRelayStatus('Connected');
-        });
-
-        _relay.on('error', () => {
-            console.log(`failed to connect to ${_relay.url}`);
-            setRelayStatus('Disconnected');
-        });
-
-        _relay.connect().then(() => {
-            let sub = _relay.sub(
-                [
-                    {
-                        kinds: [68001],
-                    }
-                ]
-            )
-            sub.on('event', event => {
-                // Extract the customer's pubkey from the incoming event
-                console.log("saw event", event)
-                const incomingPubkey = event.tags.find(tag => tag[0] === 'p')?.[1];
-                // Check if the incoming pubkey matches our pubkey
-                if (incomingPubkey !== pk) return;
-
-                const eventExists = eventFeed.find(e => e.id === event.id);
-                setEventFeed([event, ...eventFeed]);
+        async function init() {
+            console.log("running init");
+            const signer = new NDKNip07Signer();
+            let _ndk = new NDK({
+                explicitRelayUrls: RELAYS,
+                signer
             });
-        }).catch((error) => {
-            console.log(`Failed to connect to ${_relay.url}: ${error}`);
-            setRelayStatus('Failed to connect');
-        });
 
-        setRelay(_relay);
+            _ndk.pool.on("relay:connect", async (r) => {
+                console.log('connected to a relay', r.url);
+                setRelayStatus("Connected")
+            });
+            _ndk.pool.on("connect", async () => {
+                const user = await signer.user();
+                console.log('connected to something', _ndk.pool.stats());
+                // const sub = _ndk.subscribe({
+                //     since: Math.floor(Date.now() / 1000),
+                //     kinds: [68001],
+                //     '#p': [user.hexpubkey()]
+                // }, { closeOnEose: false, groupable: false });
+
+                // sub.on('event', (event: NDKEvent) => {
+                //     console.log("saw event", event.rawEvent())
+                //     // const eventExists = eventFeed.find(e => e.id === event.id);
+                //     setEventFeed([event, ...eventFeed]);
+                // })
+            });
+            _ndk.connect(2500);
+            setNDK(_ndk);
+
+            if (window.nostr === "undefined") {
+                // notify fu
+                return;
+            }
+            const pubkey = await window.nostr.getPublicKey()
+            console.log("pubkey from Alby: ", pubkey);
+            setPk(pubkey);
+
+            // const _relay = relayInit(relayUrl);
+
+            // _relay.on('connect', () => {
+            //   console.log(`connected to ${_relay.url}`);
+            //   setRelayStatus('Connected');
+            // });
+
+            // _relay.on('error', () => {
+            //   console.log(`failed to connect to ${_relay.url}`);
+            //   setRelayStatus('Disconnected');
+            // });
+
+
+
+            // _relay.connect().then(() => {
+            //   let sub = _relay.sub(
+            //     [
+            //       {
+            //         kinds: [68002],
+            //         "#p": [pk]
+            //       }
+            //     ]
+            //   )
+            //   sub.on('event', event => {
+            //     // Extract the customer's pubkey from the incoming event
+            //     console.log("saw event", event)
+            //     const eventExists = eventFeed.find(e => e.id === event.id);
+            //     setEventFeed([event, ...eventFeed]);
+            //   });
+            // }).catch((error) => {
+            //   console.log(`Failed to connect to ${_relay.url}: ${error}`);
+            //   setRelayStatus('Failed to connect');
+            // });
+
+            // setRelay(_relay);
+        }
+
+        init();
 
         return () => {
-            _relay.close();
+            if (relay) {
+                relay.close();
+            }
         };
     }, [pk, relayUrl]);
 
@@ -76,70 +123,43 @@ function App() {
         setRelayUrl(e.target.elements.relayUrl.value);
     };
 
-    // useEffect to check if there's a npub or nsec in the localstorage
-    useEffect(() => {
-        const nsec = localStorage.getItem("nsec");
-        if (nsec) {
-            setSk(nip19.decode(nsec).data);
-        }
-        const npub = localStorage.getItem("npub");
-        if (npub) {
-            setPk(nip19.decode(npub).data);
-            let short;
-            if (npub) {
-                short = npub.slice(0, 6) + "..." + npub.slice(-6);
-            } else {
-                short = "";
-            }
-            setShortNpub(short);
-        }
-    }, []);
 
-    const handleKeygen = () => {
-        const sk = generatePrivateKey();
-        localStorage.setItem("nsec", nip19.nsecEncode(sk));
-        setSk(sk);
-        const pk = getPublicKey(sk);
-        localStorage.setItem("npub", nip19.npubEncode(pk));
-        setPk(pk);
-        const npub = nip19.npubEncode(pk);
-        let short;
-        if (npub) {
-            short = npub.slice(0, 6) + "..." + npub.slice(-6);
-        } else {
-            short = "";
-        }
-        setShortNpub(short);
-
-    };
-
-    const handleRollKey = () => {
-        localStorage.removeItem("nsec");
-        localStorage.removeItem("npub");
-        setSk(null);
-        handleKeygen();
-        setEventFeed([]);
-    }
-
-    const handleSubmit = async () => {
-        if (!userInput || !sk || !relay) return;
-        let event = {
-            kind: 68002,
-            pubkey: pk,
-            created_at: Math.floor(Date.now() / 1000),
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!userInput) return;
+        const event = new NDKEvent(ndk, {
+            kind: 68005,
             tags: [
-                ["j", "summarize"]
+                ["j", "code-review"],
+                ["bid", "10000"]
             ],
             content: userInput
+        } as NostrEvent);
+
+        await event.sign();
+
+        // check if there is a subscription running here
+        if (sub !== null) {
+            console.log("already subscribed");
+            sub.stop()
         }
-        event.id = getEventHash(event);
-        event.sig = getSignature(event, sk);
-        let pub = relay.publish(event);
-        console.log("pk ", pk);
-        pub.on('ok', () => {
-            console.log(`${relay.url} has accepted our event`);
-            setUserInput("");
+
+        // create a subscription
+        const newSub = ndk?.subscribe({
+            ...event.filter()
+        }, { closeOnEose: false, groupable: false });
+        newSub!.on('event', (event: NDKEvent) => {
+            // add event to event feed
+            setEventFeed(prevEventFeed => [event, ...prevEventFeed]);
+
         })
+        setSub(newSub!);
+
+        console.log('signed_event', event.rawEvent());
+        await event.publish();
+        setUserInput("");
+
+        console.log("Set userInput to null")
     }
 
     const handleZap = async (e: Event) => {
@@ -151,7 +171,7 @@ function App() {
         await window.webln.enable();
         const invoice = e.tags.find(tag => tag[0] === 'amount')?.[2];
         const { preimage } = await window.webln.sendPayment(invoice)
-        console.log(preimage);
+
     }
 
 
@@ -171,26 +191,8 @@ function App() {
                     <button className="px-2 py-1 rounded-r-full text-gray-800 hover:bg-gray-300 transition duration-200" type="submit">Connect</button>
                 </form>
             )}
-
-            {shortNpub !== "" ? (
-                <div className="text-xl mt-4 items-center justify-between rounded-lg bg-gray-800 text-white border-white border">
-                    <button
-                        className="px-2 py-2  mx-1 hover:bg-gray-700 transition duration-200 rounded-lg"
-                        onClick={async () => await handleRollKey()}
-                    >
-                        🔄 Roll Key :
-                    </button>
-                    <span className="px-2">@{shortNpub}</span>
-                </div>
-            ) : (
-                <div className="my-4">
-                    <button
-                        className="px-2 py-1 rounded-lg bg-gray-800 text-white border-white border mx-1 hover:bg-gray-700 transition duration-200"
-                        onClick={async () => await handleKeygen()}
-                    >
-                        Gen PK
-                    </button>
-                </div>
+            {pk && (
+                <p>{nip19.npubEncode(pk)}</p>
             )}
             <form className="w-full max-w-lg flex items-center my-4 bg-white text-gray-800 rounded-full border border-white" onSubmit={handleSubmit}>
                 <input
@@ -212,25 +214,21 @@ function App() {
                 <div className="w-full max-w-lg p-4 bg-white rounded-lg text-gray-800 my-4 overflow-y-auto">
                     {eventFeed.map((event, index) => {
                         // Extract the necessary tag data
-                        const pTag = event.tags.find(tag => tag[0] === 'p')?.[1];
-                        let shortPubkey = '';
-                        if (pTag) {
-                            shortPubkey = '@' + pTag.slice(0, 6) + '...' + pTag.slice(-6);
-                        }
                         const amountSats = event.tags.find(tag => tag[0] === 'amount')?.[1] / 1000;
 
                         return (
                             <div key={index} className="border-b border-gray-200 p-2 flex justify-between items-center">
                                 <div>
-                                    <div><strong>From:</strong> {shortPubkey}</div>
+                                    <div><strong>From:</strong> {event.author.npub}</div>
                                     <div><strong>Content:</strong> {event.content}</div>
                                 </div>
-                                <button
+                                {amountSats && <button
                                     className="px-2 py-1 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition duration-200"
                                     onClick={() => handleZap(event)} // assuming you're passing the event id to handleZap
                                 >
                                     Zap ⚡ {amountSats}
                                 </button>
+                                }
                             </div>
                         )
                     })}
